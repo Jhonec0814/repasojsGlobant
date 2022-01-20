959c416b0b45b5bcb1552c503719c14f0de82781
501 <501@academicos.cesde.edu.co>
2022-01-20 09:30:16 -0500

    se crea objetos y añade fotos

---
+++ controladorLlenado.js
@@ -0,0 +1,13 @@
+let productos=[
+    {nombre:"Aguardiente tapa roja",tamano:"1/2 Botella", precio:200000, foto:"img/taparoja.jpg"},
+    {nombre:"Aguardiente tapa azul", tamano:"1/2 Botella", precio:220000, foto:"img/tapaazul.png"},
+    {nombre:"Tequila Don Julio",tamano:"1 Botella Grande", precio:300000, foto:"img/tequiladonjulio.png"},
+    {nombre:"Cerveza Aguila",tamano:"1 Botella", precio:8000, foto:"img/cervezaaguila.jpg"},
+    {nombre:"Cerveza Pilsen",tamano:"1 Botella", precio:7000, foto:"img/cervezapilsen.png"},
+    {nombre:"Ron Medellin",tamano:"1/2 Botella", precio:220000, foto:"img/ronmedellin.png"},
+    {nombre:"Ron Añejo 220 años de Caldas",tamano:"1/2 Botella", precio:100000, foto:"img/ronañejodecaldas.jpg"},
+    {nombre:"Agua",tamano:"1 Botella", precio:4000, foto:"img/agua.png"},
+    {nombre:"Soda",tamano:"1 Botella", precio:6000, foto:"img/soda.png"},
+    {nombre:"Gatorade",tamano:"1 Botella", precio:9000, foto:"img/gatorade.png"},
+    
+]
